fc44758115e90edcb39be581d340a737c0c6b40c
唐杰华 <2640427168@qq.com>
2023-11-20 14:05:39 +0800

    初始化项目

---
+++ app/extend/context.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  // 成功提示
+  apiSuccess(data = '', msg = 'ok', code = 200) {
+    this.body = { msg, data };
+    this.status = code;
+  },
+  // 失败提示
+  apiFail(data = '', msg = 'fail', code = 400) {
+    this.body = { msg, data };
+    this.status = code;
+  },
+};
---
+++ config/config.default.ts
@@ -0,0 +1,37 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1700456188801_6301';
+
+  // add your egg config in here
+  config.middleware = [ 'errorHandler' ];
+
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  config.security = {
+    // 关闭 csrf
+    csrf: {
+      enable: false,
+    },
+    // 跨域白名单
+    domainWhiteList: [ 'http://localhost:3000' ],
+  };
+  // 允许跨域的方法
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET, PUT, POST, DELETE, PATCH',
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
+};
